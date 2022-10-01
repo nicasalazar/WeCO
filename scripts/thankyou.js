c7a5c545a9b1dda004e14e543ecbe5eeb1c8a144
Wendy Kong <mkong16@my.bcit.ca>
2022-10-01 16:28:47 -0700

    added style tag and font style for every pages

--- scripts/thankyou.js
+++
@@ -1,9 +0,0 @@
-const popup = document.getElementById("thankyou-popup");
-
-function openPopup(){
-    if (document.getElementById("user_name").value != "" 
-    && document.getElementById("user_email").value != "" 
-    && document.getElementById("message").value != "") {
-        popup.classList.add("thankyou-openpopup");
-    }
-}
