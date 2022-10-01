82d314711c7cb1f0909aadbec5e547b869b9d0ad
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2022-10-01 13:52:17 -0700

    changing app.listen

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/submit-student-data', function (req, res) {
 });
 
 var server = app.listen(5000, function () {
-    console.log('Node server is running..');
+    console.log('Node server is running http://localhost:5000');
 });
 
 
