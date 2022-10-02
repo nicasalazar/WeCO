4155c9c2f7b5111af8075a5ef0321a8c8bdfa115
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2022-10-02 14:12:02 -0700

    added upload file button

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,15 @@
+function getFile() {
+  document.getElementById("upfile").click();
+}
+
+function sub(obj) {
+  var file = obj.value;
+  var fileName = file.split("\\");
+  document.getElementById("yourBtn").innerHTML = fileName[fileName.length - 1];
+  document.myForm.submit();
+  event.preventDefault();
+}
+
 function myFunction() {
     window.location.href="/views/main.html";
     onclick="getValue()";
