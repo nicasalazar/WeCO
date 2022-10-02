fe7d1f0b0bac3b268bc38528d150043717107ef0
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2022-10-01 17:59:24 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    var ampm = "";
+    m = checkTime(m);
+
+    if (h > 12) {
+    	h = h - 12;
+    	ampm = " PM";
+    } else if (h == 12){
+        h = 12;
+    	ampm = " AM";
+    } else if (h < 12){
+        ampm = " AM";
+    } else {
+        ampm = "PM";
+    };
+  
+  if(h==0) {
+    h=12;
+  }
+    
+    document.getElementById('display').innerHTML = h+":"+m+ampm;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function genMission() {
+  var randNum = Math.floor(Math.random() * 8) + 1;
+  document.getElementById('mission').innerHTML = mission[randNum];
+}
+
+//quote array
+var mission = ["Blank", "Plant a tree in each city", "Bike to work for a week", "Use eco-friendly products for a week",];
+
+//date
+function startDate() {
+  var d = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
+}
