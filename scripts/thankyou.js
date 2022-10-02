ac34fc4ae4bb157253c9d17935a20c25da96580d
Wendy Kong <mkong16@my.bcit.ca>
2022-10-02 10:30:57 -0700

    edited contactus page and fixed some footer problem

---
+++ scripts/thankyou.js
@@ -0,0 +1,9 @@
+const popup = document.getElementById("thankyou-popup");
+
+function openPopup(){
+    if (document.getElementById("user_name").value != "" 
+    && document.getElementById("user_email").value != "" 
+    && document.getElementById("message").value != "") {
+        popup.classList.add("thankyou-openpopup");
+    }
+}
